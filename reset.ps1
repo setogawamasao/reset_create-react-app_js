ce4a832ef349c2d5ddabfac5597f8abe5e762265
masaosetogawa <ms.job.hunting@gmail.com>
2020-04-12 18:13:27 +0900

    first commit

---
+++ reset.ps1
@@ -0,0 +1,27 @@
+﻿rm ../README.md
+rm ../yarn.lock
+rm ../yarn-error.log
+
+rm ../public/favicon.ico
+rm ../public/index.html
+rm ../public/logo192.png
+rm ../public/logo512.png
+rm ../public/manifest.json
+rm ../public/robots.txt
+
+Copy-Item ./public/index.html ../public
+
+rm ../src/App.css
+rm ../src/App.test.js
+rm ../src/App.js
+rm ../src/index.css
+rm ../src/index.js
+rm ../src/logo.svg
+rm ../src/react-app-env.d.js
+rm ../src/serviceWorker.js
+rm ../src/setupTests.js
+
+Copy-Item ./src/index.css ../src
+Copy-Item ./src/App.js ../src
+Copy-Item ./src/index.js ../src
+Copy-Item ./readme/README.md ../
